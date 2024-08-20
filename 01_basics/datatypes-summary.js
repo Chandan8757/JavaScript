387c9df06950332284ed3f3af632a7bc963a7329
Chandan Kumar <141899481+Chandan8757@users.noreply.github.com>
2024-08-20 15:40:26 +0000

    datatypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -9,8 +9,8 @@ const isLoggedIn = false
 const outsideTemp = null
 let userEmail;
 
-const id = symbol('123')
-const anotherId = symbol('123')
+// const id = symbol('123')
+// const anotherId = symbol('123')
 
 // console.log(id === anotherId);
 
@@ -29,9 +29,37 @@ let myObj = {
 }
 
 const myfunction = function(){
-    console.log("Hello World"); 
+   // console.log("Hello World"); 
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "Chandankumar"
+
+let anothername = myYoutubename
+anothername = "Chandan"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "ck846747@gmail.com",
+    upi: "@oksbi"
+}
+
+let userTwo = userOne
+
+userTwo.email = "ck67@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
