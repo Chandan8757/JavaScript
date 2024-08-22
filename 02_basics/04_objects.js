d1449a0556794dee70c8d773b308c080ff4b1214
Chandan Kumar <141899481+Chandan8757@users.noreply.github.com>
2024-08-22 18:39:46 +0000

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -52,8 +52,30 @@ const obj3 = {...obj1, ...obj2}
  // console.log(object.values(tinderUser));
  // console.log(object.entries(tinderUser));
 
- onsole.log(tinderUser.hasOwnProperty('isLoggedIn'));
+ // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
  
+
+ const course = {
+    coursename: "js in hindi",
+    price: "999",
+   // courseInstructor: "hitesh"
+ }
+
+ // course.courseInstructor
+
+ const {courseInstructor: instructor} = course
+
+ // console.log(courseInstructor);
+ console.log(instructor);
  
+// {
+//   "name": "Chandan",
+//   "coursename": "js in hindi",
+//   "price": "free"
+// }
  
- 
+[
+    {},
+    {},
+    {}
+]
