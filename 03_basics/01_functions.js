8e0fbc227575887702df7968937b4147b6dc1a54
Chandan Kumar <141899481+Chandan8757@users.noreply.github.com>
2024-08-22 18:56:23 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,29 @@
+
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("I"); 
+}
+
+
+
+// sayMyName()
+
+ function addTwoNumbers(number1, number2){
+
+//   console.log(number1 + number2);  
+ }
+
+function addTwoNumbers(number1, number2){
+
+    let result = number1 + number2
+    return result
+    console.log("Chandan");
+    
+}
+
+const result = addTwoNumbers(3, 4)
+
+
+console.log("Result: ", result);
