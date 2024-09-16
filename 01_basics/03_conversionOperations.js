36c5f4f620d97bdf39488f4def11a5e306e966cb
Chandan Kumar <141899481+Chandan8757@users.noreply.github.com>
2024-09-16 17:22:24 +0000

    conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,33 +1,33 @@
-let score = null
+let score = "hitesh"
 
-// console.log(typeof score);
-// console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
-let valueNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueNumber);
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
 // "33" => 33
-// " 33abc" => NaN
-// true => 1; false => 0
+// "33abc" => NaN
+//  true => 1; false => 0
 
-let isLoggedIn = "Chandan"
+let isLoggedIn = "hitesh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 =>false
+// 1 => true; 0 => false
 // "" => false
-// "Chandan" => true
+// "hitesh" => true
 
 let someNumber = 33
 
-//let stringNumber = string(someNumber)
+let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
-// ******************* Operations **************
+// *********************** Operations ***********************
 
 let value = 3
 let negValue = -value
@@ -41,7 +41,7 @@ let negValue = -value
 // console.log(2%3);
 
 let str1 = "hello"
-let str2 = " chandan"
+let str2 = " hitesh"
 
 let str3 = str1 + str2
 // console.log(str3);
@@ -51,7 +51,7 @@ let str3 = str1 + str2
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + "2");
 
-// console.log( (3+ 4) * 5 % 3);
+// console.log( (3 + 4) * 5 % 3);
 
 // console.log(+true);
 // console.log(+"");
@@ -66,6 +66,12 @@ console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
 
 
 
