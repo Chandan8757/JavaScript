8035a9bbe05e70b54cf001ffdb2c0c105c83aa7a
Chandan Kumar <141899481+Chandan8757@users.noreply.github.com>
2024-09-21 02:34:55 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,50 +1,47 @@
-//var c = 300
+// var c = 300
 let a = 300
 if (true) {
-    let a = 10
-    const b = 20
-    // console.log("INNER: ", a);
-    
-}
+   let a = 10
+   const b = 20
+//   console.log("INNER: ", a);
+
+} 
 
 
-
-// console.log(a);
+ console.log(a);
 // console.log(b);
 // console.log(c);
 
-
 function one(){
-    const username = "hitesh"
+    const username = "Chandan"
 
     function two(){
         const website = "youtube"
         console.log(username);
+        
     }
     // console.log(website);
-
-     two()
-
+    two()
 }
 
-// one()
+//one()
 
 if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
-        const website = " youtube"
-        // console.log(username + website);
-    }
-    // console.log(website);
+    const username = "Chandan"
+if (username === "Chandan") {
+    const website = "youtube"
+   // console.log(username + website);
+    
 }
+// console.log(website);
 
+}
 // console.log(username);
 
 
-// ++++++++++++++++++ interesting ++++++++++++++++++
+// ++++++++++++++++++++++++ interesting ++++++++++++
 
-
-console.log(addone(5))
+console.log(addone(5));
 
 function addone(num){
     return num + 1
@@ -52,7 +49,8 @@ function addone(num){
 
 
 
-addTwo(5)
+
 const addTwo = function(num){
     return num + 2
 }
+addTwo(5)
