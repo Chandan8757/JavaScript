55797601840a2b451c2b1b9dde26fbd3f71e33c0
Chandan Kumar <141899481+Chandan8757@users.noreply.github.com>
2024-11-25 14:05:57 +0000

    basics

--- 01_basics/06_maths.js
+++ 01_basics/06_maths.js
@@ -9,12 +9,12 @@ const balance = new Number(100)
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(4));
 
-const hundreds = 1000000
+const hundreds = 100000
 // console.log(hundreds.toLocaleString('en-IN'));
 
-// ++++++++++++ Maths +++++++++++++++++++++
+// ++++++++++++++++ Maths +++++++++++++++++++++++
 
 // console.log(Math);
 // console.log(Math.abs(-4));
@@ -24,14 +24,24 @@ const hundreds = 1000000
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
 
 const min = 10
 const max = 20
 
- console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
+
+
 
 
 
