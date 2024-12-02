81c7daebf33ed53490ca80b23150a7255e55fdce
Chandan Kumar <141899481+Chandan8757@users.noreply.github.com>
2024-12-02 13:16:10 +0000

    iterations

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -3,9 +3,9 @@
 for (let i = 0; i <= 10; i++) {
   const element = i;
   if (element == 5) {
-   //   console.log("5 is best number");
+     console.log("5 is best number");
   }
-  // console.log(element);
+   console.log(element);
   
 }
 
@@ -19,13 +19,13 @@ for (let i = 1; i <= 10; i++) {
  }
   
 }
-let myArray = ["flash", "batman", "superman"]
-console.log(myArray.length);
-for (let index = 0; index < myArray.length; index++) {
-  const element = myArray[index];
-  console.log(element);
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//   const element = myArray[index];
+//   console.log(element);
   
-}
+//}
 
 
 // break and continue
